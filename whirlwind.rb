6ec133b9af69ec04dc9c4b592d9c1f60b842fb82
Mat <mvello66@gmail.com>
2021-02-17 07:31:44 -0500

    squash intial bugs

--- whirlwind.rb
+++ whirlwind.rb
@@ -1,54 +1,52 @@
-# foods = []
-# food_index = 0
-
-# while foods.length < 5
-#   puts "give me food #{food_index + 1}"
-#   input = gets.chomp
-
-#   foods << input
-
-#   food_index += 1
+# puts "Hello!"
+# answer = gets.chomp()
+# if answer == "Arrr!"
+#   puts "Go away, pirate."
+# else
+#   puts "Greetings, hater of pirates!"
 # end
-
-# p foods
-
-# index = 0
-# while index < foods.length
-#   puts "I love #{index + 1}. #{foods[index]}"
-
-#   index += 1
+# puts "Greetings! What is your year of origin?"
+# origin = gets.chomp.to_i
+# if origin < 1900
+#   puts "That's the past!"
+# elsif origin > 1900 && origin < 2020
+#   puts "That's the present!"
+# elsif
+#   puts "That's the future!"
 # end
-
-# Hashes
-# Create a banking program that asks the user 5 times to enter a first name, last name, and email. This information should be stored as an array of hashes.
-# Each person should automatically be given an account number which is a randomized ten digit number.
-# After the user is finished, the program should print out all the users.
-# For example:
-# FIRST NAME: Joan
-# LAST NAME: Kelson
-# EMAIL: jkelson@juno.com
-# ACCT #: 2372041038
-# and so on for all the users.
-
-# users = []
-# index = 0
-
-# puts "Welcome to the user friendly banking program."
-# while users.length < 5
-#   puts "Enter a first name"
-#   first_name = gets.chomp
-#   # users << { :firstname => first_name }
-#   puts "Enter a last name"
-#   last_name = gets.chomp
-#   # users << { :last_name => last_name }
-#   puts "Enter your email"
-#   email = gets.chomp
-#   # users << { :email => email }
-#   puts "Generating account number"
-
-#   users << { :firstname => first_name, :last_name => last_name, :email => email, :account_number => rand(10) }
-
-#   index += 1
-
-#   p users
-# end
+#Exercise 5: Write a program that will average 3 numeric exam grades, return an average test score, a corresponding letter grade, and a message stating whether the student is passing.
+put "Input exam grade one:"
+exam_one = gets.chomp.to_i
+puts "Input exam grade two:"
+exam_two = gets.chomp.to_s
+puts "Input exam grade three:"
+exam_3 = gets.chomp.to_i
+def list_grade(exam_one exam_two exam_three)
+  puts "Exams: #{exam_one}, #{exam_two}, #{exam_three}"
+end
+def average_grade(exam_one, exam_two, exam_three)
+  average = (exam_one + exam_two + exam_three) / 3)
+end
+average = average_grade(exam_one, exam_two, exam_three).to_i
+def letter_grade(average-grade)
+  if average_grade < 59
+    puts "Grade: F"
+  elseif average_grade >= 60 && average_grade <= 69
+    puts "Grade: D"
+  elsif average_grade > 70 & average_grade <= 79
+    puts "Grade: C"
+  elseif average_grade >= 80 && average_grade <= 89
+    puts "Grade: B"
+  elsif average_grade >= 90
+    puts "Grade: A"
+end
+def pass_fail(average)
+  if average < 59
+    puts "Student is failing."
+  else puts "Student is passing."
+  end
+end
+list_grade(exam_one, exam_two, exam_three)
+puts "Average": #{average}"    
+lettergrade(average)   
+pass_fail(average)
